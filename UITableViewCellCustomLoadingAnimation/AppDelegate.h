b04e79544d6e8cfd49fe75fc12dad6e82990de8f
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2015-05-02 19:51:01 +0530

    Initial Commit

---
+++ UITableViewCellCustomLoadingAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewCellCustomLoadingAnimation
+//
+//  Created by Maitrayee Ghosh on 02/05/15.
+//  Copyright (c) 2015 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
